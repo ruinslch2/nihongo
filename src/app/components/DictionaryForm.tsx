6af205e18f7f8dc23e29041796a3a2313ad6d2d2
William Liao <william_liao@cyberlink.com>
2024-05-15 10:36:17 +0800

    [what] can upload dictionary.

--- src/app/components/DictionaryForm.tsx
+++ src/app/components/DictionaryForm.tsx
@@ -1,11 +1,41 @@
 import {useActionState} from "react";
+import {AxiosResponse} from "axios";
+import httpService from "../../utils/httpService.ts";
+
+interface ResponseType {
+    statusCode: number,
+    body: number
+}
+
+const uploadDict = async ({value, spell, twValue, sentence}: {
+    value: string,
+    spell: string,
+    twValue: string,
+    sentence: string
+}) => {
+    const response: AxiosResponse<ResponseType> = await httpService.post('/test/uploadMyDictionary', {
+        value,
+        spell,
+        twValue,
+        sentence
+    });
+    if (response.status === 200) {
+        return response.data.body
+    }
+    return 0
+}
 
 const DictionaryForm = () => {
-    const [error, submitAction, isPending] = useActionState(
+    const [state, submitAction, isPending] = useActionState(
         async (previousState, formData) => {
-            // const error = await updateName(formData.get("name"));
-            if (error) {
-                return error;
+            const state = await uploadDict({
+                value: formData.get("value"),
+                spell: formData.get('spell'),
+                sentence: formData.get('sentence'),
+                twValue: formData.get('tw-value')
+            });
+            if (state) {
+                return state;
             }
 
             return null;
@@ -14,18 +44,19 @@ const DictionaryForm = () => {
     );
 
     return <form action={submitAction} className="flex flex-col gap-5 border-2 rounded-2xl p-5">
-        <InputField title={'日字'} name={'ja-word'} />
-        <InputField title={'發音'} name={'ja-spell'} />
-        <InputField title={'意思'} name={'tw-word'} />
+        <InputField title={'日字'} name={'value'}/>
+        <InputField title={'發音'} name={'spell'}/>
+        <InputField title={'意思'} name={'tw-value'}/>
+        <InputField title={'例句'} name={'sentence'}/>
         <button className="rounded-xl p-3 bg-blue-500 text-white" type="submit" disabled={isPending}>Update</button>
-        {error && <p>{error}</p>}
+        {state && <p>{state}</p>}
     </form>
 }
 
-const InputField = ({title, name}:{title: string, name: string}) => {
-    return <div className={"flex flex-row items-center gap-3"}>
+const InputField = ({title, name}: { title: string, name: string }) => {
+    return <div className={"flex flex-row items-center w-full gap-3"}>
         <span>{title}:</span>
-        <input type="text" name={name} className="border-2 p-2"/>
+        <input type="text" name={name} className="border-2 p-2 w-full" required={true}/>
     </div>
 }
 
