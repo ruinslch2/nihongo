8956c467458a6b6b7c45502062421bb2196a7a32
William Liao <william_liao@cyberlink.com>
2024-05-16 11:11:29 +0800

    [what] pick the questions list randomly, show this question and options

--- src/app/Pages/SelectCardPage.tsx
+++ src/app/Pages/SelectCardPage.tsx
@@ -1,18 +1,47 @@
 import SelectGame from "../components/SelectGame.tsx";
 import TimerGame from "../components/TimerGame.tsx";
 import {getVocabularyTest} from "../../utils/apiService.ts";
-import {use} from "react";
+import {use, useMemo} from "react";
+import {getRandomObject} from "../../utils/userful.ts";
 
 
 const promiseFetchData = getVocabularyTest();
 
+export interface SelectGameQuestionType {
+    questionList: WordType[],
+    answer: WordType
+}
+
+interface WordType {
+    _id: string;
+    created_time: string;
+    sentence: string;
+    spell: string;
+    twValue: string;
+    value: string;
+}
+
 const SelectCardPage = () => {
     const {data} = use(promiseFetchData)
 
-    console.log('ddd: ', data)
+    const examList = useMemo(() => {
+        return makeQuestionList(data.data, 2);
+    }, [])
 
     return <TimerGame>
-        <SelectGame data={data.data}/>
+        <SelectGame data={examList[0]}/>
     </TimerGame>
 }
 export default SelectCardPage;
+
+const makeQuestionList = (data: WordType[], size: number) => {
+    const res = [];
+
+    for(let i=0;i<size;i++) {
+        const answerIndex = Math.floor(Math.random()*4)
+        const randomObjList = getRandomObject<WordType>(data, 4);
+        res.push({answer: randomObjList[answerIndex], questionList: randomObjList})
+    }
+
+    return res
+}
--- src/app/components/SelectGame.tsx
+++ src/app/components/SelectGame.tsx
@@ -1,21 +1,14 @@
 import Card from "./Card.tsx";
 import {useState} from "react";
+import {SelectGameQuestionType} from "../Pages/SelectCardPage.tsx";
 
-interface WordType {
-    sentence: string;
-    spell: string;
-    twValue: string;
-    value: string;
-    _id: string;
-}
-
-const SelectGame = ({data}: { data: WordType[] }) => {
+const SelectGame = ({data}: { data: SelectGameQuestionType }) => {
     const [selectedCardId, setSelectedCardId] = useState<number>();
-
+    const {questionList, answer} = data;
 
     return <div className={'grid grid-flow-row grid-cols-2 gap-5'}>
-
-        {data.map((d, index) => (
+        <span className="col-span-2">{answer.value}</span>
+        {questionList.map((d, index) => (
             <Card key={d._id} className={selectedCardId === index ? 'border-blue-300' : ''} onClick={() => setSelectedCardId(index)}>{d.spell}</Card>
         ))}
     </div>
