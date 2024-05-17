e82e51951e93d671cc39f31f36e87e044f14260f
William Liao <william_liao@cyberlink.com>
2024-05-17 15:44:39 +0800

    [what] beautify the route link css

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -1,6 +1,7 @@
 import {use} from "react";
 import RouteCard from "./components/RouteCard.tsx";
 import {fetchDictSize} from "../utils/apiService.ts";
+import bookIcon from "../assets/icon-book.png";
 import NihonChart from "./components/NihonChart.tsx";
 
 
@@ -8,17 +9,17 @@ const promiseFetchData = fetchDictSize();
 
 const HomePage = () => {
 
-    const {data} = use(promiseFetchData);
+    // const {data} = use(promiseFetchData);
 
-    return <div className="grid grid-flow-row grid-cols-3 gap-5 w-full justify-center p-5">
-        <div className={"col-span-3 mx-auto p-5 rounded-2xl border w-full"}>
-            <div className={'w-1/3'}><NihonChart /></div>
-            目前已學了{data.data}個單字, N3完成度
-            <span className="text-red-400">{(data.data / 3750).toFixed(2)}%</span>
-        </div>
-        <RouteCard title="學習" subTitle="增加字典" link={"/nihongo/add-dictionary"}/>
-        <RouteCard title="Timer Game" link={"/nihongo/timer-game"}/>
-        <RouteCard title="Timer Game" link={"/nihongo/timer-game"}/>
+    return <div className="grid grid-flow-row grid-cols-1 gap-5 w-full justify-center p-5">
+        {/*<div className={"col-span-3 mx-auto p-5 rounded-2xl border w-full"}>*/}
+        {/*    <div className={'w-1/3'}><NihonChart /></div>*/}
+        {/*    目前已學了{data.data}個單字, N3完成度*/}
+        {/*    <span className="text-red-400">{(data.data / 3750).toFixed(2)}%</span>*/}
+        {/*</div>*/}
+        <RouteCard title="辞書" subTitle="語彙を増やす" link={"/nihongo/add-dictionary"} icon={bookIcon} color={'bg-red-200'}/>
+        <RouteCard title="Timer Game" link={"/nihongo/timer-game"} icon={""} color={'bg-blue-200'}/>
+        {/*<RouteCard title="Timer Game" link={"/nihongo/timer-game"} icon={bookIcon}/>*/}
     </div>
 }
 
--- src/app/components/RouteCard.tsx
+++ src/app/components/RouteCard.tsx
@@ -1,18 +1,39 @@
 import {Link} from "react-router-dom";
 import Card from "./Card.tsx";
-const RouteCard = ({className, title, subTitle, link}: {className?: string, title: string, subTitle?: string, link: string }) => {
-    return <Card className={`rounded-2xl mx-auto w-[150px] h-[150px] border-2 p-5 flex flex-col ${className}`}>
-        <div className={"relative flex flex-col h-full"}>
-            <header className="text-2xl">{title}</header>
-            <article className="text-base">{subTitle}</article>
-            <Link
-                className="absolute bottom-0 right-0 py-3.75 block w-1/2 select-none cursor-pointer rounded border"
-                to={link}
-            >
-                Go
-            </Link>
-        </div>
-    </Card>
+
+const RouteCard = ({className, title, subTitle, link, icon, color}: {
+    className?: string,
+    title: string,
+    subTitle?: string,
+    link: string,
+    icon?: string,
+    color: string
+}) => {
+    return (
+        <Card
+            className={`relative rounded-2xl mx-auto w-[300px] border border-black h-[70px] flex flex-row shadow-xl p-0 ${className}`}>
+            {icon && <div className="z-10 w-10 h-10 absolute top-[-20px] left-[10px]">
+                <img src={icon} alt={'icon'}/>
+            </div>
+            }
+            <div className={"relative overflow-hidden rounded-2xl px-5 flex flex-row items-center gap-5 h-full"}>
+                <header className="z-10 text-2xl">{title}</header>
+                <article className="z-10 text-base text-gray-600">{subTitle}</article>
+                <div className={"absolute right-0 h-full"}>
+                    <Link
+                        className="h-full flex group select-none cursor-pointer "
+                        to={link}
+                    >
+                        <div
+                            className={`w-4 h-full right-[-5px] pointer-events-none absolute z-1 scale-x-1 group-hover:scale-x-[40] duration-500 ${color}`}>
+                        </div>
+                        <span className="relative my-auto mr-5">出発</span>
+                    </Link>
+                </div>
+
+            </div>
+        </Card>
+    )
 }
 
 export default RouteCard;
