b198e3ec509ca04a65686d12b0c4efc3c51c4f09
William Liao <william_liao@cyberlink.com>
2024-05-23 16:26:54 +0800

    [what] river component init

--- src/app/components/River.tsx
+++ src/app/components/River.tsx
@@ -1,9 +1,8 @@
 const River = () => {
-
     return <div className="overflow-hidden w-full h-full">
-        {/*<div className="grid grid-flow-col grid-rows-4 gap-2 animate-marquee flex-wrap">*/}
-
-        {/*</div>*/}
+        <div className="grid grid-flow-col grid-rows-4 gap-2 animate-marquee flex-wrap">
+            <div>123</div>
+        </div>
     </div>
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,12 @@ export default {
   theme: {
     extend: {
       animation: {
-        marquee: 'marquee 25s linear infinite',
-        marquee2: 'marquee2 25s linear infinite',
+        marquee: 'marquee 5s linear infinite',
       },
       keyframes: {
         marquee: {
           '0%': { transform: 'translateX(0%)' },
           '100%': { transform: 'translateX(-100%)' },
-        },
-        marquee2: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0%)' },
         },
       },
     },
