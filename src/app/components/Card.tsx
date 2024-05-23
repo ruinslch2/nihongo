abb6b70afdd3fd7e8cecc8c73ba10c2dd3e96438
William Liao <william_liao@cyberlink.com>
2024-05-23 17:17:49 +0800

    Updates

--- src/app/components/Card.tsx
+++
@@ -1,8 +0,0 @@
-const Card: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({className, children, ...props}) => {
-    return <div
-        className={`rounded-2xl flex flex-col justify-center border shadow-xl ${className}`} {...props}>
-        {children}
-    </div>
-}
-
-export default Card;
--- src/app/components/River.tsx
+++
@@ -1,9 +0,0 @@
-const River = () => {
-    return <div className="overflow-hidden w-full h-full">
-        <div className="grid grid-flow-col grid-rows-4 gap-2 animate-marquee flex-wrap">
-            <div>123</div>
-        </div>
-    </div>
-}
-
-export default River;
