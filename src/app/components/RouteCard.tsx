67587062ec6f8df808b050303639181fc936498c
William Liao <william_liao@cyberlink.com>
2024-05-24 14:49:48 +0800

    [what] change the route rule:  only click "Go" btn => can click this card everywhere [why] better UX [how] as what

--- src/app/components/RouteCard.tsx
+++ src/app/components/RouteCard.tsx
@@ -10,29 +10,25 @@ const RouteCard = ({className, title, subTitle, link, icon, color}: {
     color: string
 }) => {
     return (
-        <Card
-            className={`relative rounded-2xl border border-black h-[70px] flex flex-row p-0 ${className}`}>
-            {icon && <div className="z-10 w-10 h-10 absolute top-[-20px] left-[10px]">
-                <img src={icon} alt={'icon'}/>
-            </div>
-            }
-            <div className={"relative overflow-hidden rounded-2xl px-5 flex flex-row items-center gap-5 h-full"}>
-                <header className="z-10 text-2xl">{title}</header>
-                <article className="z-10 text-base text-gray-600">{subTitle}</article>
-                <div className={"absolute right-0 h-full"}>
-                    <Link
-                        className="h-full flex group select-none cursor-pointer "
-                        to={link}
-                    >
-                        <div
-                            className={`w-4 h-full right-[-5px] pointer-events-none absolute z-1 scale-x-1 group-hover:scale-x-[80] duration-500 ${color}`}>
-                        </div>
-                        <span className="relative my-auto mr-5">出発</span>
-                    </Link>
+        <Link
+            className="group select-none cursor-pointer"
+            to={link}
+        >
+            <Card
+                className={`relative rounded-2xl border border-black h-[70px] flex flex-row p-0 ${className}`}>
+                {icon && <div className="z-10 w-10 h-10 absolute top-[-20px] left-[10px]">
+                    <img src={icon} alt={'icon'}/>
                 </div>
-
-            </div>
-        </Card>
+                }
+                <div className={"relative overflow-hidden rounded-2xl px-5 flex flex-row items-center gap-5 h-full"}>
+                    <header className="z-10 text-2xl">{title}</header>
+                    <article className="z-10 text-base text-gray-600">{subTitle}</article>
+                    <div
+                        className={`w-4 h-full right-[-5px] pointer-events-none absolute z-1 scale-x-1 group-hover:scale-x-[80] duration-500 ${color}`}>
+                    </div>
+                </div>
+            </Card>
+        </Link>
     )
 }
 
